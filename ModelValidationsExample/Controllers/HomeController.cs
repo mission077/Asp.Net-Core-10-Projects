282e516189f4d3e55097f909528110d468bd5fd4
Mission Kadariya <mis8202@hytrol.com>
2025-07-10 08:41:03 -0500

    Created a Model class and controller and used model validation attribute for validation

---
+++ ModelValidationsExample/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using ModelValidationsExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModelValidationsExample.Controllers;
+public class HomeController : Controller
+{
+    [Route("register")]
+    public IActionResult Index(Person _person)
+    {
+        return Content($"Person:{_person}");
+    }
+}
