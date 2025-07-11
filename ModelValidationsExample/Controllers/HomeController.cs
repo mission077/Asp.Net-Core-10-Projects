e6436590b2535c027dbcba8a55a845980532529b
Mission Kadariya <mis8202@hytrol.com>
2025-07-11 16:46:55 -0500

    Added Model Validations Controller

--- ModelValidationsExample/Controllers/HomeController.cs
+++ ModelValidationsExample/Controllers/HomeController.cs
@@ -7,6 +7,19 @@ public class HomeController : Controller
     [Route("register")]
     public IActionResult Index(Person _person)
     {
+        if(ModelState.IsValid == false)
+        {
+            List<string> ErrorList = new List<string>();
+            foreach (var values in ModelState.Values)
+            {
+                foreach (var error in values.Errors)
+                {
+                    ErrorList.Add(error.ErrorMessage);
+                }
+                string.Join("/n", ErrorList);
+            }
+            return BadRequest();
+        }
         return Content($"Person:{_person}");
     }
 }
